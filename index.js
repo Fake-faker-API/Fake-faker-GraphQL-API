a5636bdc79ed8ebd021b6ec241fbbc67192fba86
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-23 16:21:47 -0500

    Update port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require("./graphqlLayer/typeDefs");
 const { resolvers } = require("./graphqlLayer/resolvers");
+const config = require("./lib/config");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.listen({ port: 4000 }).then(({ url }) => {
+server.listen({ port: config.PORT ||  4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
