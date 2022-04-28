b701d93faee5c30b0e19b220c47c348bb7c6a645
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-04-28 13:40:55 -0500

    Added plugin to load GQL playground

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const { ApolloServer } = require('apollo-server');
-const { typeDefs } = require("./graphql/typeDefs/typeDefs");
-const { resolvers } = require("./graphql/resolvers/resolvers");
-const config = require("./lib/config/config");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground
+} = require('apollo-server-core');
+const { typeDefs } = require('./graphql/typeDefs/typeDefs');
+const { resolvers } = require('./graphql/resolvers/resolvers');
+const config = require('./lib/config/config');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: true
+  introspection: true,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 });
 
 server.listen({ port: config.PORT ||  4000 }).then(({ url }) => {
